622e235aa52d4a5b322d43a2f80d0e05fda75171
会飞 <huifei@huifeideMBP.lan>
2023-09-24 15:36:49 +0800

    feat: 添加 Background 管理主现场

--- src/main/index.js
+++ src/main/index.js
@@ -2,6 +2,11 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
+import { initIpcMain } from './ipcMain'
+import clc from 'cli-color'
+const log = (text) => {
+  console.log(`${clc.blueBright('[background.js]')} ${text}`)
+}
 
 function createWindow() {
   // Create the browser window.
@@ -28,7 +33,7 @@ function createWindow() {
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    console.log(1,process.env['ELECTRON_RENDERER_URL'])
+    console.log(1, process.env['ELECTRON_RENDERER_URL'])
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
     console.log(2)
@@ -39,45 +44,90 @@ function createWindow() {
     console.log(data)
   })
   ipcMain.on('setTitle', (e, data) => {
-    const child = new BrowserWindow({parent: mainWindow, modal: true})
+    const child = new BrowserWindow({ parent: mainWindow, modal: true })
     child.loadURL('http://127.0.0.1:5174/')
     child.show()
     console.log('setTitle', data, child)
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
-
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
-
-  createWindow()
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
+class Background {
+  constructor() {
+    this.init()
+  }
+  init() {
+    log('initializing')
+    this.handleAppEvents()
+  }
+  handleAppEvents() {
+    app.on('ready', async () => {
+      log('app ready event')
+      initIpcMain()
+      this.createWindow()
+    })
+    // Set app user model id for windows
+    electronApp.setAppUserModelId('com.electron')
+
+    // Default open or close DevTools by F12 in development
+    // and ignore CommandOrControl + R in production.
+    // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+    app.on('browser-window-created', (_, window) => {
+      optimizer.watchWindowShortcuts(window)
+    })
+
+    app.on('activate', function () {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+    // Quit when all windows are closed, except on macOS. There, it's common
+    // for applications and their menu bar to stay active until the user quits
+    // explicitly with Cmd + Q.
+    app.on('window-all-closed', () => {
+      if (process.platform !== 'darwin') {
+        app.quit()
+      }
+    })
+  }
+  createWindow() {
+    log('creating app window')
+    const options = {
+      width: 900,
+      height: 670,
+      titleBarStyle: 'hiddenInset',
+      title: 'yun music',
+      show: false,
+      webPreferences: {
+        webSecurity: false,
+        nodeIntegration: true,
+        enableRemoteModule: true,
+        contextIsolation: false,
+        preload: join(__dirname, '../preload/index.js'),
+        sandbox: false
+      }
+    }
+    this.window = new BrowserWindow(options)
+    this.window.on('ready-to-show', () => {
+      this.window.show()
+    })
+
+    this.window.webContents.setWindowOpenHandler((details) => {
+      shell.openExternal(details.url)
+      return { action: 'deny' }
+    })
+    this.window.setMenuBarVisibility(false)
+
+    if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+      console.log(1, process.env['ELECTRON_RENDERER_URL'])
+      this.window.loadURL(process.env['ELECTRON_RENDERER_URL'])
+    } else {
+      console.log(2)
+      this.window.loadFile(join(__dirname, '../renderer/index.html'))
+    }
+  }
+}
+
+new Background()
